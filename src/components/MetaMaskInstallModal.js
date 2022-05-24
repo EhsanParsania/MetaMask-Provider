f1c75d5c3494fc84474bdb40ebda89d3d3f01f5f
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-05-24 20:38:53 +0300

    change install modal style

--- src/components/MetaMaskInstallModal.js
+++ src/components/MetaMaskInstallModal.js
@@ -7,7 +7,7 @@ export const MetaMaskInstallModal = () => {
   const [modalShow, setModalShow] = useState(false);
   return (
     <>
-      <Button className=" mt-2 p-2" type='button' onClick={() => setModalShow(true)}>
+      <Button className=" mt-2 p-2 " type='button' onClick={() => setModalShow(true)}>
         Install MetaMask Wallet
       </Button>
       <ConnectModal
@@ -32,29 +32,31 @@ function ConnectModal(props) {
     onboarding.startOnboarding();
   }
   return (
-    <Modal
+   <div className="bg-dark">
+     <Modal
       {...props}
       size="md"
       aria-labelledby="contained-modal-title-vcenter"
       contentClassName='bg-dark p-1'
       centered
     >
-      <Modal.Header className={`btn text-left text-white  cursor-default `}>
+      <Modal.Header  className={` text-white d-flex justify-content-end  cursor-default `}>
         <button className='btn btn-danger text-center border-0 ' onClick={props.onHide}><span className='font-weight-bold'>×</span></button>
       </Modal.Header>
-      <Modal.Body className="d-flex justify-content-center p-4 my-3 bg-dark">
+      <Modal.Body className="d-flex justify-content-center p-4 pb-0 my-2 bg-dark">
         <main className=" site-content text-center col-10 mb-5" >
           <div className="site-content__center ">
             <h3 className="text-warning ">
               Attention !
             </h3>
             <p className="text-warning" style={{ fontSize: '19px' }} >
-              You should reload the page after installing the wallet
+              You should reload the page after installing MetaMask
             </p>
-            <Button onClick={handleConnectToWallet}>Start Onboarding MetaMask</Button>
+            <Button className="btn btn-secondary mt-3" onClick={handleConnectToWallet}>Start Onboarding MetaMask</Button>
           </div>
         </main>
       </Modal.Body>
-    </Modal>
+    </Modal> 
+   </div>
   );
 }
