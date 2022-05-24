bb5e9d42b5b98ff0eb342cbb4ddc368838a0c000
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-05-24 13:19:09 +0300

    add default react style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { MetaMask } from './components/MetaMask';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    <div className='container'>
-
-      <MetaMask/>
+    <div className="App">
+      <header className="App-header">
+        <div className='container'>
+          <MetaMask />
+        </div>
+      </header>
     </div>
-    //   </header>
-    // </div>
   );
 }
 
