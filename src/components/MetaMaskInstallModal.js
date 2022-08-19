391a3654cae2787d9919d03216c580c75a8b567a
EhsanParsania <ehsan.parsa69@gmail.com>
2022-08-19 17:04:08 +0300

    support IOS and Android devices

--- src/components/MetaMaskInstallModal.js
+++ src/components/MetaMaskInstallModal.js
@@ -31,32 +31,52 @@ function ConnectModal(props) {
   const handleConnectToWallet = () => {
     onboarding.startOnboarding();
   }
+
+  const isMobileDevice = () => {
+    return 'ontouchstart' in window || 'onmsgesturechange' in window;
+  }
+
   return (
-   <div className="bg-dark">
-     <Modal
-      {...props}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      contentClassName='bg-dark p-1'
-      centered
-    >
-      <Modal.Header  className={` text-white d-flex justify-content-end  cursor-default `}>
-        <button className='btn btn-danger text-center border-0 ' onClick={props.onHide}><span className='font-weight-bold'>×</span></button>
-      </Modal.Header>
-      <Modal.Body className="d-flex justify-content-center p-4 pb-0 my-2 bg-dark">
-        <main className=" site-content text-center col-10 mb-5" >
-          <div className="site-content__center ">
-            <h3 className="text-warning ">
-              Attention !
-            </h3>
-            <p className="text-warning" style={{ fontSize: '19px' }} >
-              You should reload the page after installing MetaMask
-            </p>
-            <Button className="btn btn-secondary mt-3" onClick={handleConnectToWallet}>Start Onboarding MetaMask</Button>
-          </div>
-        </main>
-      </Modal.Body>
-    </Modal> 
-   </div>
+    <div className="bg-dark">
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        contentClassName='bg-dark p-1'
+        centered
+      >
+        <Modal.Header className={` text-white d-flex justify-content-end  cursor-default `}>
+          <button className='btn btn-danger text-center border-0 ' onClick={props.onHide}><span className='font-weight-bold'>×</span></button>
+        </Modal.Header>
+        <Modal.Body className="d-flex justify-content-center p-4 pb-0 my-2 bg-dark">
+          <main className=" site-content text-center col-10 mb-5" >
+            <div className="site-content__center ">
+              <h3 className="text-warning ">
+                Attention !
+              </h3>
+              <p className="text-warning" style={{ fontSize: '19px' }} >
+                You should reload the page after installing MetaMask
+              </p>
+              {
+                isMobileDevice() ?
+                  <ConnectToPhoneMetaMask />
+                  : <Button onClick={handleConnectToWallet}>Install Wallet</Button>
+              }
+            </div>
+          </main>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+
+function ConnectToPhoneMetaMask({ setUserAddress }) {
+  const YOUR_ORIGIN = process.env.REACT_APP_FRONTEND_ORIGIN
+  const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + YOUR_ORIGIN ;   // put your origin here without http:// ro https://
+  return (
+    <a href={metamaskAppDeepLink}>
+      <Button >Connect to MetaMask</Button>
+    </a>
   );
 }
